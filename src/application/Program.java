9d2f8729c452ed890729de245d55253eb73336ab
marciogama <marciogama2000@gmail.com>
2019-10-11 15:46:35 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -23,5 +23,12 @@ public class Program {
 		for (Seller obj : list)	{
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n=== TEST 3: seller findAll implementation =====");
+		list = sellerDao.findAll();
+		for (Seller obj : list)	{
+			System.out.println(obj);
+		}
+
 	}
 }
